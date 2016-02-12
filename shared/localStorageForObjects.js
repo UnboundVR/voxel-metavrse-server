6ae6de3aba64621016d11645d807c9887d040d1c
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-02-12 03:01:45 -0300

    removing the local storage for objects helper

--- shared/localStorageForObjects.js
+++
@@ -1,8 +0,0 @@
-Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
-};
-
-Storage.prototype.getObject = function(key) {
-    var value = this.getItem(key);
-    return value && JSON.parse(value);
-};
