85b9be2d6646b4f94555e379ecdcb1c96c1d5784
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-02-17 04:30:29 -0300

    hiding the player when in first person view

--- client/controls.js
+++ client/controls.js
@@ -2,13 +2,31 @@ var fly = require('voxel-fly');
 var walk = require('voxel-walk');
 
 module.exports = function(game, avatar) {
+  var avatarVisible;
+
+  function setAvatarVisibility(visible) {
+    avatar.playerSkin.rightArm.visible = visible;
+    avatar.playerSkin.leftArm.visible = visible;
+    avatar.playerSkin.rightLeg.visible = visible;
+    avatar.playerSkin.leftLeg.visible = visible;
+    avatar.playerSkin.head.visible = visible;
+    avatar.playerSkin.body.visible = visible;
+
+    avatarVisible = visible;
+  }
+
+
   var makeFly = fly(game);
   var target = game.controls.target();
   game.flyer = makeFly(target);
+  setAvatarVisibility(false);
 
   // toggle between first and third person modes
   window.addEventListener('keydown', function (ev) {
-    if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle();
+    if (ev.keyCode === 'R'.charCodeAt(0)) {
+      avatar.toggle();
+      setAvatarVisibility(!avatarVisible);
+    }
   });
 
   game.on('tick', function() {
