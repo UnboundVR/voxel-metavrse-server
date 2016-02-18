c17f7937718a671347da769c4cba3f060c931904
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-02-18 18:58:02 -0300

    adding storage for gists

--- server/coding.js
+++ server/coding.js
@@ -1,19 +1,37 @@
-var gists = {};
+var storage = require('./storage');
+
+var gists;
+var dirty = false;
+
+function saveGists() {
+  if(dirty) {
+    storage.saveGists(gists);
+    dirty = false;
+  }
+}
 
 module.exports = function(io) {
-  io.on('connection', function(socket) { // TODO use different namespace in socket.io
-    socket.on('requestGists', function(callback) {
-      callback(gists);
-    });
+  storage.loadGists().then(function(res) {
+    gists = res;
 
-    socket.on('codeChanged', function(position, gistId) {
-      gists[position] = gistId;
-      socket.broadcast.emit('codeChanged', position, gistId);
-    });
+    setInterval(saveGists, 1000); // 1s
 
-    socket.on('codeRemoved', function(position) {
-      delete gists[position];
-      socket.broadcast.emit('codeRemoved', position);
+    io.on('connection', function(socket) { // TODO use different namespace in socket.io
+      socket.on('requestGists', function(callback) {
+        callback(gists);
+      });
+
+      socket.on('codeChanged', function(position, gistId) {
+        gists[position] = gistId;
+        socket.broadcast.emit('codeChanged', position, gistId);
+        dirty = true;
+      });
+
+      socket.on('codeRemoved', function(position) {
+        delete gists[position];
+        socket.broadcast.emit('codeRemoved', position);
+        dirty = true;
+      });
     });
   });
 };
--- server/storage.js
+++ server/storage.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var Promise = require('promise');
 
-var basePath = process.cwd() + '/chunks';
+var basePath = process.cwd() + '/storage';
+var gistsPath = basePath + '/gists.json';
 
 function getPath(chunkId) {
   return basePath + '/' + chunkId;
@@ -37,6 +38,30 @@ module.exports = {
       fs.readFile(getPath(chunkId), function(err, data) {
         if(err) {
           resolve(undefined);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      });
+    });
+  },
+  saveGists: function(gists) {
+    return new Promise(function(resolve, reject) {
+      ensureDirectoryExists().then(function() {
+        fs.writeFile(gistsPath, JSON.stringify(gists), function(err) {
+          if(err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    });
+  },
+  loadGists: function() {
+    return new Promise(function(resolve, reject) {
+      fs.readFile(gistsPath, function(err, data) {
+        if(err) {
+          resolve({});
         } else {
           resolve(JSON.parse(data));
         }
