741318f74591b22b04d0c4edd633bf2894986164
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-03-05 02:19:44 -0300

    fixing bug with voxel store

--- server/services/store/voxels.js
+++ server/services/store/voxels.js
@@ -2,9 +2,9 @@ var fileStorage = require('./fileStorage')('chunks');
 
 module.exports = {
   saveChunk: function(chunkId, chunk) {
-    return fileStorage.save('chunks/' + chunkId, chunk);
+    return fileStorage.save(chunkId, chunk);
   },
   loadChunk: function(chunkId) {
-    return fileStorage.load('chunks/' + chunkId);
+    return fileStorage.load(chunkId);
   }
 };
