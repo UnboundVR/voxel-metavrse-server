4d3d49184452e63aee520f073d571f9d4a31fd01
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-03-06 06:10:31 -0300

    adding tests for expandGists

--- shared/coding/expandGists.js
+++ shared/coding/expandGists.js
@@ -4,12 +4,12 @@ module.exports = function(gists, getGist) {
   var promises = [];
   Object.keys(gists).forEach(function(position) {
     promises.push(getGist(gists[position]).then(function(codeObj) {
-      return extend(codeObj, {position: position});
+      return extend({}, codeObj, {position: position});
     }));
   });
 
-  var result = {};
   return Promise.all(promises).then(function(codeObjs) {
+    var result = {};
     codeObjs.forEach(function(codeObj) {
       result[codeObj.position] = {
         id: codeObj.id,
---
+++ spec/shared/expandGistsSpec.js
@@ -0,0 +1,42 @@
+var test = require('tape');
+var expandGists = require('../../shared/coding/expandGists');
+var expandedGists = {
+  'id1': {id: 'id1', code: 'asd'},
+  'id2': {id: 'id2', code: 'wasd'}
+};
+var expandedGistsResult = {
+  'pos1': expandedGists['id1'],
+  'pos2': expandedGists['id2']
+};
+
+test('expandGists with successful getGist', function(t) {
+  var gists = {
+    'pos1': 'id1',
+    'pos2': 'id2'
+  };
+
+  var getGist = function(id) {
+    return Promise.resolve(expandedGists[id]);
+  };
+
+  expandGists(gists, getGist).then(function(result) {
+    t.deepEqual(result, expandedGistsResult, 'returns expanded gists by position');
+    t.end();
+  });
+});
+
+test('expandGists with faulty getGist', function(t) {
+  var gists = {
+    'pos1': 'id1',
+    'pos2': 'id2'
+  };
+
+  var getGist = function(id) {
+    return Promise.reject('error');
+  };
+
+  expandGists(gists, getGist).catch(function(err) {
+    t.pass('Gets error: ' + err);
+    t.end();
+  });
+});
