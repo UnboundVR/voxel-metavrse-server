7434de76939d7a9c5727746b7d4485ee388e4264
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-03-20 20:47:09 -0300

    minimum refactor of chatservice

--- client/chat/service.js
+++ client/chat/service.js
@@ -2,23 +2,18 @@ var io = require('socket.io-client');
 var EventEmitter2 = require('eventemitter2').EventEmitter2;
 var util = require('util');
 
-var socket;
-
-function ChatService() {
-
-}
+function ChatService() {}
 
 ChatService.prototype.init = function() {
-  socket = io.connect(location.host + '/chat');
-  socket.on('message', function(message) {
+  this.socket = io.connect(location.host + '/chat');
+  this.socket.on('message', function(message) {
     this.emit('message', message);
   });
 };
 
 ChatService.prototype.init = function(message) {
-  socket.emit('message', message);
+  this.socket.emit('message', message);
 };
 
 util.inherits(ChatService, EventEmitter2);
-
 module.exports = new ChatService();
