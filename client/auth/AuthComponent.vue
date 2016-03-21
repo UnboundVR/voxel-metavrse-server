a9e2e27174dded8a2df5ba80d3a75dd06027ae84
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-03-20 22:54:59 -0300

    migrating auth to vue component

---
+++ client/auth/AuthComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="userInfo">
+    <h3>User info</h3>
+    <button v-if="loggedIn" v-on:click="logout">Logout</button>
+    <button v-else v-on:click="login">Login</button>
+    <span v-if="loggedIn">{{name}}</span>
+  </div>
+</template>
+
+<script>
+
+import service from './service';
+
+export default {
+  name: 'AuthComponent',
+  data() {
+    return {
+      loggedIn: service.isLoggedIn(),
+      name: service.getName()
+    };
+  },
+  methods: {
+    login: service.login,
+    logout: service.logout
+  }
+};
+</script>
+
+<style>
+#userInfo {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+</style>
--- client/auth/index.js
+++ client/auth/index.js
@@ -1,69 +1,13 @@
 import Vue from 'vue';
-import querystring from 'querystring';
-import githubAuth from './githubAuth';
-import tokenStore from './tokenStore';
-
-var accessToken;
-var vm;
-
-function login() {
-  githubAuth.getLoginUrl().then(function(url) {
-    location.href = url;
-  });
-}
-
-function logout() {
-  tokenStore.deleteToken();
-  location.href = location.origin;
-}
-
-function fetchUserData() {
-  return githubAuth.getLoggedUserInfo(accessToken).then(function(me) {
-    vm.name = me.name;
-    vm.loggedIn = true;
-  });
-}
+import AuthComponent from './AuthComponent.vue';
+import service from './service';
 
 export default {
-  getName() {
-    return vm.name;
-  },
-  getAccessToken() {
-    return accessToken;
-  },
-  isLogged() {
-    return !!accessToken;
-  },
+  getName: service.getName,
+  getAccessToken: service.getAccessToken,
+  isLogged: service.isLoggedIn,
   init() {
-    vm = new Vue({
-      el: '#userInfo',
-      data: {
-        loggedIn: false,
-        name: ''
-      },
-      methods: {
-        login: login,
-        logout: logout
-      }
-    });
-
-    if(tokenStore.hasToken()) {
-      accessToken = tokenStore.getToken();
-      return fetchUserData();
-    }
-
-    var qs = querystring.parse(location.search.substring(1)); // TODO check state too
-
-    if(qs.code) {
-      return githubAuth.getAccessToken(qs.code).then(function(token) {
-        accessToken = token;
-        tokenStore.storeToken(accessToken);
-        return fetchUserData();
-      }, function(err) {
-        alert(err);
-      });
-    } else {
-      return Promise.resolve();
-    }
+    Vue.component('auth-component', AuthComponent);
+    return service.init();
   }
 };
