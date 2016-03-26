915243e49287336412bafe9ae20604ad61d721db
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-03-26 13:02:14 -0300

    WIP remaking voxel controller

--- server/voxel/controller.js
+++ server/voxel/controller.js
@@ -1,12 +1,17 @@
 var Promise = require('promise');
-var storage = require('./store');
+// var storage = require('./store');
 var settings = require('../../shared/voxelSettings.json');
+
+var chunks = [];
 
 module.exports = {
   init: function() {
-    
+
   },
   initClient: function() {
+    var chunkPositions = [];
+
+
     return {
       chunks: []
     };
--- spec/server/voxelControllerSpec.js
+++ spec/server/voxelControllerSpec.js
@@ -1,9 +1,46 @@
 var test = require('tape');
+var Promise = require('promise');
 var sinon = require('sinon');
 var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
-var setup = function() {
+var mockStore;
+var settings = {
+  chunkDistance: 2,
+  worldOrigin: [0, 0, 0]
+};
+var CHUNK_SIZE = 32;
+
+var setup = function(success) {
+  mockStore = {
+    saveChunk: sinon.stub().returns(success ? Promise.resolve() : Promise.reject('error')),
+    loadChunk: sinon.spy(function(position) {
+      if(!success) {
+        return Promise.reject('error');
+      }
+
+      return Promise.resolve({
+        dims: [CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE],
+        position: position,
+        voxels: new Array(CHUNK_SIZE*CHUNK_SIZE*CHUNK_SIZE).fill(0)
+      });
+    })
+  };
 
   return proxyquire('../../server/voxel/controller', {
+    './store': mockStore,
+    '../../shared/voxelSettings.json': settings
   });
 };
+
+test('VoxelController::initClient when load works fine', function(t) {
+  var controller = setup(true);
+  controller.initClient().then(function(result) {
+    t.equals(result.size, 16, 'contains correct amount of chunks');
+    t.end();
+  });
+});
+
+test('VoxelController::initClient when load fails', function(t) {
+  var controller = setup(false);
+  t.end();
+});
