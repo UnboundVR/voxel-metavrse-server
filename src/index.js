0fbf4e7608347199a9eaea8cd8f582309cf0214a
Mauro Julián Fernández <mauro@unboundvr.com>
2016-04-04 16:54:24 -0300

    This shouldn't be here.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import r from 'rethinkdb';
 import restify from 'restify';
 import socketio from 'socket.io';
 
@@ -6,19 +5,6 @@ let server = restify.createServer();
 server.use(restify.CORS());
 
 let io = socketio.listen(server.server);
-
-// r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
-//   if(err) throw err;
-//   r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
-//     if(err) throw err;
-//     console.log(res);
-//     r.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res)
-//     {
-//       if(err) throw err;
-//       console.log(res);
-//     });
-//   });
-// });
 
 // require('use-strict'); // TODO: uncomment after fixing strict issues with libraries
 require('dotenv').load();
