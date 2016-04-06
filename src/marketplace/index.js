cf394cde1c88034885cf0093f2eb7ee4838d1118
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-04-06 02:49:23 -0300

    adding TODO note

--- src/marketplace/index.js
+++ src/marketplace/index.js
@@ -10,6 +10,7 @@ export default function(server) {
     });
   });
 
+  // TODO automatically notify users that the new blockType was created. We must ensure there is no race condition with the setBlock that we do after this...
   router.post('/blockType', (req, res) => {
     var body = JSON.parse(req.body); // TODO automatically send the stuff parsed...
     controller.addBlockType(req.params.token, body.code, body.material).then(function(blockType) {
