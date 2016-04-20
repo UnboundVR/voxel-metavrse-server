f4b9c25541dae37e6f37197e188d1f1c45f7e15c
Mauro Julián Fernández <mauro@unboundvr.com>
2016-04-20 03:17:32 -0300

    es6 fixes and new function.

--- src/voxel/storage.js
+++ src/voxel/storage.js
@@ -1,17 +1,27 @@
+import Promise from 'bluebird';
 import r from 'rethinkdb';
 
 export default {
   getEmptyChunkTable(dbConn) {
-    return new Promise(function(resolve, reject) {
-      r.table('chunk').isEmpty().run(dbConn, function(error, data) {
+    return new Promise((resolve, reject) => {
+      r.table('chunk').isEmpty().run(dbConn, (error, data) => {
         resolve(data);
       });
     });
   },
   saveChunks(dbConn, chunks) {
-    return new Promise(function(resolve, reject) {
-      r.table('chunk').insert(chunks).run(dbConn, function(error, data) {
+    return new Promise((resolve, reject) => {
+      r.table('chunk').insert(chunks).run(dbConn, (error, data) => {
+        if (error) reject(error);
         resolve(data);
+      });
+    });
+  },
+  getChunks(dbConn) {
+    return new Promise((resolve, reject) => {
+      r.table('chunk').run(dbConn, (error, data) => {
+        if (error) reject(error);
+        resolve(data.toArray());
       });
     });
   }
