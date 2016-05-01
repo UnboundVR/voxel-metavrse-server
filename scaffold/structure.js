4aad729e7f60af2628ed901139f9a3b72034efaa
Mauro Julián Fernández <mauro@unboundvr.com>
2016-05-01 17:47:01 -0300

    Fix for when db doesn't exist

--- scaffold/structure.js
+++ scaffold/structure.js
@@ -1,8 +1,12 @@
 import r from 'rethinkdb';
 
 export default async function createStructure(conn) {
-  await r.dbDrop('metavrse').run(conn);
-  console.log('dropped db');
+  try {
+    await r.dbDrop('metavrse').run(conn);
+    console.log('dropped db');
+  } catch (e) {
+    console.log(`Database doesn't exist, we'll create it`);
+  }
 
   await r.dbCreate('metavrse').run(conn);
   console.log('created db');
